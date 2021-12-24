2fe0cdd1b8826f97667c43383ba65c9a00496072
Maksim Bazhenov <maksim4k1@gmail.com>
2021-12-25 01:13:52 +0600

    added default data for db

--- db/defaultData.js
+++ db/defaultData.js
@@ -1,16 +1,523 @@
 module.exports = {
-  users: [
+  "users": [
     {
-      token: 'token_bhje73bkj38jlds9',
-      data: {
-        username: 'admin',
-        firstName: 'John',
-        age: 18,
+      "token": "token_bhje73bkj38jlds9",
+      "data": {
+        "username": "admin",
+        "name": "John",
+        "description": "",
+        "score": 23,
+        "image": "",
+        "myQuizzes": [
+          "HDuyjhJH&5",
+          "HDuyjhJH&4",
+          "HDuyjhJH&6",
+          "quiz_FHfYzsBSW"
+        ]
       },
-      auth: {
-        username: 'admin',
-        password: '1234',
+      "auth": {
+        "username": "admin",
+        "password": "1234"
       }
+    },
+    {
+      "auth": {
+        "username": "Maks",
+        "password": "1234"
+      },
+      "data": {
+        "username": "Maks",
+        "name": "Баженов Максим",
+        "score": 9,
+        "description": "",
+        "image": "",
+        "myQuizzes": []
+      },
+      "token": "token_zrsxQc7TS"
+    },
+    {
+      "auth": {
+        "username": "maks",
+        "password": "1234"
+      },
+      "data": {
+        "username": "maks",
+        "name": "Maksim",
+        "score": 8,
+        "description": "",
+        "image": "",
+        "myQuizzes": []
+      },
+      "token": "token_ZY7h9qlV_"
+    },
+    {
+      "auth": {
+        "username": "maksim4k1",
+        "password": "1234"
+      },
+      "data": {
+        "username": "maksim4k1",
+        "name": "Maks",
+        "description": "Front-End разработчик",
+        "score": 7,
+        "image": "",
+        "myQuizzes": []
+      },
+      "token": "token_AF2eColk4"
+    },
+    {
+      "auth": {
+        "username": "maksim",
+        "password": "1234"
+      },
+      "data": {
+        "username": "maksim",
+        "name": "Баженов Максим",
+        "score": 5,
+        "description": "",
+        "image": "",
+        "myQuizzes": []
+      },
+      "token": "token_41GzTo8Zv"
+    },
+    {
+      "auth": {
+        "username": "_a2l4_",
+        "password": "1234"
+      },
+      "data": {
+        "username": "_a2l4_",
+        "name": "Александррр",
+        "score": 2,
+        "description": "",
+        "image": "",
+        "myQuizzes": []
+      },
+      "token": "token_XJ_J5ilK9"
+    }
+  ],
+  "categories": [
+    {
+      "id": "4g4joeu70",
+      "title": "Математика",
+      "description": "Викронины на знание основ алгебры и геометрии. Реши задачи и узнай свой уровень в математике!"
+    },
+    {
+      "id": "41111kkd71",
+      "title": "Чудеса света",
+      "description": "Кто постоил пирамиды? Почему Пизанкая башня стоит криво? Где находиться Атлантида? Это и многое другое вы узнаете пройдя викторину."
+    },
+    {
+      "id": "21hyGGDet2",
+      "title": "Фильмы и мультфильмы",
+      "description": "Пройди наши викторины и похвастайся своими знаниями в фильмах и мультфильмах!"
+    },
+    {
+      "id": "JDruhj47hf",
+      "title": "История",
+      "description": "Проверь звои знания и узнай когда и при каких обстоятельствах произошли самые интересные события в истории!"
+    }
+  ],
+  "quizzes": [
+    {
+      "id": "HuywGG6e3",
+      "categoryId": "4g4joeu70",
+      "quizzes": [
+        {
+          "id": "HDuyjhJH&5",
+          "title": "Теорема Виетта",
+          "description": "Викторина на умение находить корни уравнения через теорему Виетта и через дискриминант"
+        },
+        {
+          "id": "HDuyjhJH&4",
+          "title": "Сложение и вычитание",
+          "description": "Сложение и вычитание многозачных чисел"
+        }
+      ]
+    },
+    {
+      "id": "HuywGG6e5",
+      "categoryId": "41111kkd71",
+      "quizzes": []
+    },
+    {
+      "id": "HuywGG6e6",
+      "categoryId": "JDruhj47hf",
+      "quizzes": [
+        {
+          "id": "HDuyjhJH&6",
+          "title": "История",
+          "description": "Проверь звои знания и узнай когда и при каких обстоятельствах произошли самые интересные события в истории!"
+        },
+        {
+          "id": "quiz_FHfYzsBSW",
+          "title": "История России",
+          "description": "Знай свою историю!"
+        }
+      ]
+    },
+    {
+      "id": "HuywGG6e2",
+      "categoryId": "21hyGGDet2",
+      "quizzes": []
+    }
+  ],
+  "quiz": [
+    {
+      "id": "HDuyjhJH&5",
+      "name": "Теорема Виетта",
+      "description": "Викторина на умение находить корни уравнения через теорему Виетта и через дискриминант.",
+      "category": "4g4joeu70",
+      "author": "admin",
+      "questions": [
+        {
+          "question": "Решите пример: 2 + 2 = ...",
+          "answers": [
+            {
+              "answer": "3",
+              "isTrue": false
+            },
+            {
+              "answer": "4",
+              "isTrue": true
+            },
+            {
+              "answer": "8",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Решите пример: 4 + 5 = ...",
+          "answers": [
+            {
+              "answer": "9",
+              "isTrue": true
+            },
+            {
+              "answer": "5",
+              "isTrue": false
+            },
+            {
+              "answer": "8",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Решите пример: 8 * 7 = ...",
+          "answers": [
+            {
+              "answer": "15",
+              "isTrue": false
+            },
+            {
+              "answer": "64",
+              "isTrue": false
+            },
+            {
+              "answer": "56",
+              "isTrue": true
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        }
+      ],
+      "results": [
+        {
+          "username": "maks",
+          "result": 3
+        },
+        {
+          "username": "maksim4k1",
+          "result": 3
+        },
+        {
+          "username": "Maks",
+          "result": 3
+        },
+        {
+          "username": "admin",
+          "result": 1
+        },
+        {
+          "username": "maksim",
+          "result": 3
+        }
+      ]
+    },
+    {
+      "id": "HDuyjhJH&4",
+      "name": "Сложение и вычитание",
+      "description": "Сложение и вычитание многозачных чисел",
+      "category": "4g4joeu70",
+      "author": "admin",
+      "questions": [
+        {
+          "question": "Решите пример: 2 + 2 = ...",
+          "answers": [
+            {
+              "answer": "3",
+              "isTrue": false
+            },
+            {
+              "answer": "4",
+              "isTrue": true
+            },
+            {
+              "answer": "8",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Решите пример: 4 + 5 = ...",
+          "answers": [
+            {
+              "answer": "9",
+              "isTrue": true
+            },
+            {
+              "answer": "5",
+              "isTrue": false
+            },
+            {
+              "answer": "8",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Решите пример: 8 * 7 = ...",
+          "answers": [
+            {
+              "answer": "15",
+              "isTrue": false
+            },
+            {
+              "answer": "64",
+              "isTrue": false
+            },
+            {
+              "answer": "56",
+              "isTrue": true
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        }
+      ],
+      "results": [
+        {
+          "username": "maksim4k1",
+          "result": 2
+        },
+        {
+          "username": "Maks",
+          "result": 3
+        },
+        {
+          "username": "maks",
+          "result": 2
+        },
+        {
+          "username": "maksim",
+          "result": 2
+        },
+        {
+          "username": "_a2l4_",
+          "result": 2
+        },
+        {
+          "username": "admin",
+          "result": 1
+        }
+      ]
+    },
+    {
+      "id": "HDuyjhJH&6",
+      "name": "История",
+      "description": "Проверь звои знания и узнай когда и при каких обстоятельствах произошли самые интересные события в истории!",
+      "category": "JDruhj47hf",
+      "author": "admin",
+      "questions": [
+        {
+          "question": "Решите пример: 2 + 2 = ...",
+          "answers": [
+            {
+              "answer": "3",
+              "isTrue": false
+            },
+            {
+              "answer": "4",
+              "isTrue": true
+            },
+            {
+              "answer": "8",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Решите пример: 4 + 5 = ...",
+          "answers": [
+            {
+              "answer": "9",
+              "isTrue": true
+            },
+            {
+              "answer": "5",
+              "isTrue": false
+            },
+            {
+              "answer": "8",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Решите пример: 8 * 7 = ...",
+          "answers": [
+            {
+              "answer": "15",
+              "isTrue": false
+            },
+            {
+              "answer": "64",
+              "isTrue": false
+            },
+            {
+              "answer": "56",
+              "isTrue": true
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        }
+      ],
+      "results": [
+        {
+          "username": "Maks",
+          "result": 3
+        },
+        {
+          "username": "maks",
+          "result": 2
+        },
+        {
+          "username": "maksim4k1",
+          "result": 2
+        }
+      ]
+    },
+    {
+      "id": "quiz_FHfYzsBSW",
+      "name": "История России",
+      "category": "JDruhj47hf",
+      "author": "admin",
+      "description": "Знай свою историю!",
+      "questions": [
+        {
+          "question": "Какая столица России?",
+          "answers": [
+            {
+              "answer": "Санкт-Петербург",
+              "isTrue": false
+            },
+            {
+              "answer": "Киев",
+              "isTrue": false
+            },
+            {
+              "answer": "Москва",
+              "isTrue": true
+            },
+            {
+              "answer": "Нью-Йорк",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "В каком году Наполеон напал на Россию?",
+          "answers": [
+            {
+              "answer": "1816 год",
+              "isTrue": false
+            },
+            {
+              "answer": "1812 год",
+              "isTrue": true
+            },
+            {
+              "answer": "1912 год",
+              "isTrue": false
+            },
+            {
+              "answer": "1813 год",
+              "isTrue": false
+            }
+          ]
+        },
+        {
+          "question": "Как называлась государственная политика, объявленная в Советском Союзе в 1985 году?",
+          "answers": [
+            {
+              "answer": "Перестройка",
+              "isTrue": true
+            },
+            {
+              "answer": "Первая пятилетка",
+              "isTrue": false
+            },
+            {
+              "answer": "Дефолт",
+              "isTrue": false
+            },
+            {
+              "answer": "Нет правильного ответа",
+              "isTrue": false
+            }
+          ]
+        }
+      ],
+      "results": [
+        {
+          "username": "admin",
+          "result": 2
+        }
+      ]
     }
   ]
 }
